442b46fa719a0ecd512e2e45774f7fbc8ee49fd1
nathaniel-wendt <nathaniel.wendt@gmail.com>
2018-11-18 19:06:10 -0700

    feat: add GoogleMaps API connectivity

--- src/App.js
+++ src/App.js
@@ -2,13 +2,42 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.renderMap()
+  }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAp2UAcZ8h6HlJ_J3Ha9qZcbt6ohSR9yFs&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.1672, lng: -105.1019},
+      zoom: 14
+    });
+  }
+
+
+
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <main>
+        <div id="map"></div>
+      </main>
     );
   }
+}
+
+/* learned from Yahya Elharony's YouTube Video: http://www.youtube.com/watch?v=W5LhLZqj76s */
+function loadScript(url) {
+  let index = window.document.getElementsByTagName("script")[0]
+  let script = window.document.createElement('script')
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
 }
 
 export default App;
