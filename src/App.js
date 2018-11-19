c69db717e049d6a76a19861c819431107ba49186
nathaniel-wendt <nathaniel.wendt@gmail.com>
2018-11-18 21:34:02 -0700

    feat: add location state and fetch from API

--- src/App.js
+++ src/App.js
@@ -3,23 +3,39 @@ import './App.css';
 
 class App extends Component {
 
+  state = {
+    locations: []
+  }
+
   componentDidMount() {
     this.renderMap()
+    this.fetchLocData()
+  }
+
+  initMap = () => {
+    new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.1672, lng: -105.1019},
+      zoom: 14
+    });
   }
 
   renderMap = () => {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAp2UAcZ8h6HlJ_J3Ha9qZcbt6ohSR9yFs&callback=initMap")
     window.initMap = this.initMap
   }
-
-  initMap = () => {
-    const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 40.1672, lng: -105.1019},
-      zoom: 14
-    });
+  
+  fetchLocData = () => {
+    fetch('https://api.myjson.com/bins/f948q')
+    .then(response => response.json())
+    .then(data =>
+      this.setState({
+        locations: data
+      })
+    )
+    .catch(err => {
+      console.log(err);
+    })
   }
-
-
 
   render() {
     return (
