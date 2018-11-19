db93030369f4ea87cf66c3a9a2300ad119ae30e7
nathaniel-wendt <nathaniel.wendt@gmail.com>
2018-11-18 22:00:24 -0700

    feat: add Google Map Markers

--- src/App.js
+++ src/App.js
@@ -8,14 +8,22 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.renderMap()
     this.fetchLocData()
   }
 
   initMap = () => {
-    new window.google.maps.Map(document.getElementById('map'), {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 40.1672, lng: -105.1019},
-      zoom: 14
+      zoom: 13
+    });
+
+    this.state.locations.map(location => {
+
+      let marker = new window.google.maps.Marker({
+        position: {lat: location.pos.lat, lng: location.pos.lng},
+        map: map,
+        title: location.name
+      })
     });
   }
 
@@ -23,14 +31,14 @@ class App extends Component {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAp2UAcZ8h6HlJ_J3Ha9qZcbt6ohSR9yFs&callback=initMap")
     window.initMap = this.initMap
   }
-  
+
   fetchLocData = () => {
     fetch('https://api.myjson.com/bins/f948q')
     .then(response => response.json())
     .then(data =>
       this.setState({
         locations: data
-      })
+      },  this.renderMap())
     )
     .catch(err => {
       console.log(err);
